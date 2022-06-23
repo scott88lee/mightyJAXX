b9d55705b0d5bece364967ca8fe2df19cad28c91
Scott Lee <scott88lee@gmail.com>
2022-06-24 06:59:31 +0800

    ENV variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env['PORT'] || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
