9076d3673ad0b4d4f860faffe387aad4cf1cfb90
Scott Lee <scott88lee@gmail.com>
2022-06-24 07:31:12 +0800

    Setup MongoDB

---
+++ db/connection.js
@@ -0,0 +1,33 @@
+let { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+let uri = process.env['DB_URI'] || console.log('Missing DB ENV variable.\n')
+
+let mongodb;
+let client;
+
+function connect(callback) {
+    MongoClient.connect(uri, (err, db) => {
+        if (err) { console.log(err) }
+        else {
+            console.log('MongoDB connected.')
+            client = db;
+            mongodb = db.db('test');
+            callback();
+        }
+    });
+}
+
+function query() {
+    return mongodb;
+}
+
+function mongoClient() {
+    return client;
+}
+
+module.exports = {
+    connect,
+    query,
+    mongoClient
+};
