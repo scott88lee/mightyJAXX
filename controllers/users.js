860b53240a40130695dbb3de9af4f5ef2b120dea
Scott Lee <scott88lee@gmail.com>
2022-06-24 17:26:57 +0800

    parameter validations

--- controllers/users.js
+++ controllers/users.js
@@ -6,8 +6,21 @@ const listAll = async (req, res) => {
 }
 
 const createNew = async (req, res) => {
+    const v = require('../helpers/validators');
     let body = req.body;
-    console.log(req.body);
+    
+    if (!v.emailValidation(body.email)) {
+        res.json({Success: false, Message: 'Invalid email'});
+        return;
+    }
+    if (!v.passwordLength(body.password)){
+        res.json({Success: false, Message: 'Password must be at least 8 characters long'});
+        return;
+    }
+    if (!v.passwordCombination(body.password)) {
+        res.json({Success: false, Message: 'Password must contain at least one uppercase letter, one lowercase letter, one number and one special character'});
+        return;
+    }
     
     res.send('Ok');
 }
---
+++ helpers/validators.js
@@ -0,0 +1,18 @@
+module.exports = {
+    passwordLength: (password) => {
+        return password.length >= 8;
+    },
+
+    passwordCombination: (password) => {
+        // Must contain upper and lowercase letters,
+        // numbers and special characters cannot contain spaces
+        let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return regex.test(password);
+    },
+
+    emailValidation: (email) => {
+        //check valid email string
+        let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
+    }
+}
