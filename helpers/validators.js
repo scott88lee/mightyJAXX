96971f85bfa0b5e7a42c88b96f79c0985c8daf76
Scott Lee <scott88lee@gmail.com>
2022-06-29 17:28:01 +0800

    final fix

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,25 +1,34 @@
-export const passwordLength = (password) => {
+const passwordLength = (password) => {
     return password.length >= 8;
 }
 
-export const containsNumber = (password) => {
+const containsNumber = (password) => {
     return /\d/.test(password);
 }
 
-export const containsUpperCase = (password) => {
+const containsUpperCase = (password) => {
     return /[A-Z]/.test(password);
 }
 
-export const containsLowerCase = (password) => {
+const containsLowerCase = (password) => {
     return /[a-z]/.test(password);
 }
 
-export const containsSpecialChar = (password) => {
+const containsSpecialChar = (password) => {
     return /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password);
 }
 
-export const emailString = (email) => {
+const emailString = (email) => {
     //check valid email string
     let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
+}
+
+module.exports = {
+    passwordLength,
+    containsNumber,
+    containsUpperCase,
+    containsLowerCase,
+    containsSpecialChar,
+    emailString
 }
